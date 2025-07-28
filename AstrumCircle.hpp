d60a792f86f56e5f29369346494756b5228f5ebd
zunonuz <jyunni614@naver.com>
2025-07-28 10:52:28 +0900

    Collider에 더블 디스패치 디자인 적용

--- AstrumCircle.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "AstrumVector2.hpp"
-
-struct AstrumCircle {
-    AstrumVector2 Center;
-    float Radius;
-};
--- AstrumRotatedRect.cpp
+++
@@ -1,9 +0,0 @@
-#include "AstrumRotatedRect.hpp"
-
-AstrumRotatedRect::AstrumRotatedRect(const AstrumRect& rect)
-{
-    LeftBottom = rect.LeftBottom;
-    RightTop = rect.RightTop;
-    RightBottom = { rect.RightTop.X, rect.LeftBottom.Y };
-    LeftTop = { rect.LeftBottom.X, rect.RightTop.Y };
-}
